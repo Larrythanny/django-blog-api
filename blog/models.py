051b5db05110a7c92aa37858fa5b31f102ea9181
Larry Thanny <musiliuolanrewaju69@gmail.com>
2026-04-17 00:45:31 -0700

    Updated blog Api with edit features

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Blog(models.Model):
     content = models.TextField()
     author_name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now= True)
 
     def __str__(self):
         return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
-from .views import blog_list_create
 
-urlpatterns = [
-    path('blogs/', blog_list_create),
-]
+from rest_framework.routers import DefaultRouter
+from .views import BlogViewSet
+
+router = DefaultRouter()
+router.register(r'blogs', BlogViewSet)
+
+urlpatterns = router.urls
+
+
+
+# from django.urls import path
+# from .views import blog_list_create
+
+# urlpatterns = [
+#     path('blogs/', blog_list_create),
+# ]
